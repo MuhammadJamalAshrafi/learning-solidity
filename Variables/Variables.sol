363f3b1b3acb7ffc9a561118c07963e6e4029cd1
Muhammad Jamal Ashrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2023-04-03 11:56:50 +0500

    Update Variables.sol

--- Variables/Variables.sol
+++ Variables/Variables.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 contract Variables {
     // State variables are stored on the blockchain.
-    string public text = "Hello World!!!";
+    string public text = "Hello";
     uint public num = 123;
 
-    function doSomething() public view returns(uint256) {
+    function doSomething() public {
         // Local variables are not saved to the blockchain.
-        uint256 i = 456;
-        return i;
+        uint i = 456;
 
         // Here are some global variables
         uint timestamp = block.timestamp; // Current block timestamp
